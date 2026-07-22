1c752af6d57a0ef5d451f2a779b4fe56f74eaf03
Shaurya9935 <prakhargupta9935@gmail.com>
2026-07-23 00:05:30 +0530

    completed task 10

--- src/10-pincode-checker.js
+++ src/10-pincode-checker.js
@@ -59,20 +59,51 @@
  */
 export function getDataType(value) {
   // Your code here
+  if(value === null) {
+    return "null";
+  }
+  else if(Array.isArray(value)){
+    return "array"
+  }
+  else {
+    return typeof value
+  }
 }
 
 export function isValidParcelWeight(weight) {
   // Your code here
+  if(!Number.isFinite(weight) || weight <= 0){
+    return false;
+  } else {
+    return true;
+  }
 }
 
 export function isWholeNumber(value) {
   // Your code here
+  if(!Number.isInteger(value)){
+    return false;
+  }
+  else {
+    return true;
+  }
 }
 
 export function isNotANumber(value) {
   // Your code here
+  if(!Number.isNaN(value)){
+    return false;
+  }
+  else{
+    return true;
+  }
 }
 
 export function isTruthy(value) {
   // Your code here
+  if(Boolean(value)){
+    return true;
+  }else {
+    return false;
+  }
 }
